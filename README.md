2faa6051e60f90923a8f150f48596b7fa5e651af
Zarzelcow <62089453+Zarzelcow@users.noreply.github.com>
2022-09-09 06:28:03 -0700

    💎 Initial Commit 💎

---
+++ README.md
@@ -0,0 +1,3 @@
+# Compact Sine LUT
+
+Backport for the Compact Sine LUT algorithm in the [Lithium](https://github.com/CaffeineMC/lithium-fabric) mod by JellySquid and 2No2Name
---
+++ src/main/java/com/github/zarzelcow/compactsinelut/mixin/MixinMathHelper.java
@@ -0,0 +1,27 @@
+package com.github.zarzelcow.compactsinelut.mixin;
+
+import com.github.zarzelcow.compactsinelut.CompactSineLUT;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(MathHelper.class)
+public class MixinMathHelper {
+    /**
+     * @author jellysquid3
+     * @reason use an optimized implementation
+     */
+    @Overwrite
+    public static float sin(float f) {
+        return CompactSineLUT.sin(f);
+    }
+
+    /**
+     * @author jellysquid3
+     * @reason use an optimized implementation
+     */
+    @Overwrite
+    public static float cos(float f) {
+        return CompactSineLUT.cos(f);
+    }
+}
